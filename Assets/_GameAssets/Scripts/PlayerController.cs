4cf7994b59ee7eb2d21a9c46d500724c2785bc62
Ertugrul Civici <0certugrul2@gmail.com>
2026-06-04 00:52:04 +0300

    v0.05 Character Controller Added

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -5,30 +5,46 @@ using UnityEngine.Rendering;
 public class PlayerController : MonoBehaviour
 {
     [Header("References")]
-   [ SerializeField] private Transform _Orientationtransform;
 
+   [ SerializeField] private Transform _Orientationtransform;
     [Header("Movement Settings")]
-    [SerializeField] private float _PlayerSpeed = 10f;
+    [SerializeField] private KeyCode _PlayerSpeedKey;
+    [SerializeField] private float _PlayerSpeed;
+
+
+
+
+
+    [Header("Sliding Settings")]
+
+    [SerializeField] private KeyCode _SlideKey;
+    [SerializeField] private float _SlideMultiplier;
+    [SerializeField] private float _SlideDrag;
+    
 
 
     [Header("Jump Settings")]
+
     [SerializeField] private KeyCode _JumpKey;
+    [SerializeField] private float _JumpForce ;
+    [SerializeField] private float _JumpCooldown ;
+    [SerializeField] private bool _CanJump;
     
-    [SerializeField] private float _JumpForce = 5f;
-    [SerializeField] private float _JumpCooldown = 1f;
-    
-    [SerializeField] private bool _CanJump= true;
-    
+
+
+
+
     [Header("Ground Check Settings")]
     [SerializeField] private float playerHeight;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float _GroundDrag;
 
 
     
 
 
 
-
+    private bool _isSliding;
     private Rigidbody rb;
 
     private float horizontalInput, verticalInput;
@@ -45,6 +61,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         SetInputs();
+        SetPlayerDrag();
+        LimitPlayerSpeed();
 
     }
     private void FixedUpdate()
@@ -54,22 +72,34 @@ public class PlayerController : MonoBehaviour
 
     private void SetInputs()
     {
-        horizontalInput = 
+        {horizontalInput = 
         Keyboard.current.aKey.isPressed ? -1 
         : Keyboard.current.dKey.isPressed ? 1 : 0;
 
         verticalInput = 
         Keyboard.current.wKey.isPressed ? 1 
-        : Keyboard.current.sKey.isPressed ? -1 : 0;
+        : Keyboard.current.sKey.isPressed ? -1 : 0;}
 
-
-        if(Keyboard.current.spaceKey.wasPressedThisFrame && _CanJump && IsGrounded())
+       
+       if(Keyboard.current.spaceKey.wasPressedThisFrame && _CanJump && IsGrounded())
         {
              _CanJump = false;
             
             PlayerJump();
              Invoke(nameof(ResetJump), _JumpCooldown);
         }
+        else if(Keyboard.current.leftShiftKey.isPressed)
+        {
+        _isSliding = true;
+        }
+        else 
+        {
+        _isSliding = false;
+        }
+        
+        
+        
+        
         
         
 }
@@ -80,9 +110,35 @@ public class PlayerController : MonoBehaviour
          _Orientationtransform.forward * verticalInput + 
         _Orientationtransform.right * horizontalInput;
 
+            if(_isSliding)
+            {
+                rb.AddForce(movementDirection.normalized*_PlayerSpeed*_SlideMultiplier, ForceMode.Force);
+            }
+            else
         rb.AddForce(movementDirection.normalized*_PlayerSpeed, ForceMode.Force);
 
 
+    }
+    private void SetPlayerDrag()
+    {
+       if(_isSliding)
+
+        {
+            rb.linearDamping = _SlideDrag;
+        }
+        else
+        {
+            rb.linearDamping = _GroundDrag;
+        }
+    }
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.z);
+        if(flatVelocity.magnitude > _PlayerSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * _PlayerSpeed;
+            rb.linearVelocity = new Vector3(limitedVelocity.x, rb.linearVelocity.y, limitedVelocity.z);
+        }
     }
 
     private void PlayerJump()
@@ -103,5 +159,6 @@ public class PlayerController : MonoBehaviour
     {
         return Physics.Raycast(transform.position, Vector3.down, playerHeight*0.5f + 0.2f,groundLayer);
     }
+
 
 }
