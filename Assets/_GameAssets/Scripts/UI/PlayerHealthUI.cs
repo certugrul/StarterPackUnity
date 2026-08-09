572388afb23310c098a954dc23223f4817b92ca3
Ertugrul Civici <certugrulx@gmail.com>
2026-08-09 17:41:24 +0300

    v0.13 Timer System Added

--- Assets/_GameAssets/Scripts/UI/PlayerHealthUI.cs
+++ Assets/_GameAssets/Scripts/UI/PlayerHealthUI.cs
@@ -1,77 +1,82 @@
-using UnityEngine.UI;
-using UnityEngine;
-using DG.Tweening;
 using System;
+using DG.Tweening;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerHealthUI : MonoBehaviour
 {
-        [Header("Referances")]
-        [SerializeField] private Image[] _playerHealthImages;
-        
+    [Header("Referances")]
+    [SerializeField]
+    private Image[] _playerHealthImages;
 
-        [Header("Sprites")]
-        [SerializeField] private Sprite _playerHealtySprite;
-        [SerializeField] private Sprite _playerUnhealtySprite;       
-        
-        [Header("Settings")]
+    [Header("Sprites")]
+    [SerializeField]
+    private Sprite _playerHealtySprite;
 
-        [SerializeField] private float _scaleDuration;
+    [SerializeField]
+    private Sprite _playerUnhealtySprite;
 
-        private RectTransform[] _playerHealthTransform;
+    [Header("Settings")]
+    [SerializeField]
+    private float _scaleDuration;
+
+    private RectTransform[] _playerHealthTransform;
 
     private void Awake()
     {
-        _playerHealthTransform= new RectTransform[_playerHealthImages.Length];
+        _playerHealthTransform = new RectTransform[_playerHealthImages.Length];
 
-        for(int i = 0; i <_playerHealthImages.Length; i++)
+        for (int i = 0; i < _playerHealthImages.Length; i++)
         {
-            _playerHealthTransform[i]=_playerHealthImages[i].gameObject.GetComponent<RectTransform>();
+            _playerHealthTransform[i] = _playerHealthImages[i]
+                .gameObject.GetComponent<RectTransform>();
         }
-         
     }
+
     //ForTest
     private void Update()
     {
         if (Keyboard.current.oKey.wasPressedThisFrame)
-    {
-        AnimateDamage();
+        {
+            AnimateDamage();
+        }
+
+        if (Keyboard.current.pKey.wasPressedThisFrame)
+        {
+            AnimateDamageForAll();
+        }
     }
 
-    if (Keyboard.current.pKey.wasPressedThisFrame)
-    {
-        AnimateDamageForAll();
-    }
-    }
     public void AnimateDamage()
     {
         for (int i = 0; i < _playerHealthImages.Length; i++)
         {
             if (_playerHealthImages[i].sprite == _playerHealtySprite)
             {
-                AnimateDamageSprite(_playerHealthImages[i],_playerHealthTransform[i]);
+                AnimateDamageSprite(_playerHealthImages[i], _playerHealthTransform[i]);
                 break;
             }
         }
     }
+
     public void AnimateDamageForAll()
     {
         for (int i = 0; i < _playerHealthImages.Length; i++)
         {
-          
-                AnimateDamageSprite(_playerHealthImages[i],_playerHealthTransform[i]);
-            
-            
+            AnimateDamageSprite(_playerHealthImages[i], _playerHealthTransform[i]);
         }
     }
+
     private void AnimateDamageSprite(Image activeImage, RectTransform activeImageTransform)
     {
-        activeImageTransform.DOScale(0f, _scaleDuration).SetEase(Ease.InBack).OnComplete(() =>
-        {
-            activeImage.sprite=_playerUnhealtySprite;
-            activeImageTransform.DOScale(1f, _scaleDuration).SetEase(Ease.OutBack);
-        });
+        activeImageTransform
+            .DOScale(0f, _scaleDuration)
+            .SetEase(Ease.InBack)
+            .OnComplete(() =>
+            {
+                activeImage.sprite = _playerUnhealtySprite;
+                activeImageTransform.DOScale(1f, _scaleDuration).SetEase(Ease.OutBack);
+            });
     }
-    
-
 }
