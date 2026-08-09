eb31214188a33ced7f73c541ececf9ade0be1e2e
Ertugrul Civici <certugrulx@gmail.com>
2026-08-09 18:49:39 +0300

    v0.14a Consts Update

--- Assets/_GameAssets/Scripts/Helpers/Consts.cs
+++ Assets/_GameAssets/Scripts/Helpers/Consts.cs
@@ -5,9 +5,9 @@ public class Consts
         public const string IS_MOVING = "IsMoving";
         public const string IS_JUMPING = "IsJumping";
         public const string IS_SLIDING = "IsSliding";
-        public const string IS_SLIDING_ACTIVE = "isSlidingActive";
+        public const string IS_SLIDING_ACTIVE = "IsSlidingActive";
     }
-    
+
     public struct WheatTypes
     {
         public const string GOLD_WHEAT = "GoldWheat";
