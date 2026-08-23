dd373d678f615801793d50e2eba5004edcaea646
Ertugrul Civici <certugrulx@gmail.com>
2026-08-24 00:34:41 +0300

    v0.18 Damageables Fires Added

--- Assets/_GameAssets/Scripts/Managers/HealthManager.cs
+++ Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -1,34 +1,49 @@
+using System;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    [SerializeField] private int _maxHealth=3;
+    public static HealthManager Instance { get; private set; }
+    public event Action OnPlayerDeath;
+
+    [Header("References")]
+    [SerializeField]
+    private PlayerHealthUI _playerHealthUI;
+
+    [Header("Settings")]
+    [SerializeField]
+    private int _maxHealth = 3;
     private int _currentHealth;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
-        _currentHealth=_maxHealth;
-        
+        _currentHealth = _maxHealth;
     }
-    public void Damage(int damage)
+
+    public void Damage(int damageAmount)
     {
         if (_currentHealth > 0)
         {
-            _currentHealth-=damage;
-            //UI ANIMATE DAMAGE TODO
+            _currentHealth -= damageAmount;
+            _playerHealthUI.AnimateDamage();
+
             if (_currentHealth <= 0)
             {
-                Destroy(gameObject);
+                OnPlayerDeath?.Invoke();
             }
-
         }
     }
+
     public void Heal(int healAmount)
     {
         if (_currentHealth < _maxHealth)
         {
-            _currentHealth=Mathf.Min(_currentHealth + healAmount, _maxHealth);
+            _currentHealth = Mathf.Min(_currentHealth + healAmount, _maxHealth);
         }
     }
-
 }
--- Assets/_GameAssets/Scripts/Player/PlayerInteractionController.cs
+++ Assets/_GameAssets/Scripts/Player/PlayerInteractionController.cs
@@ -2,28 +2,38 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _playerVisualTransform;
     private PlayerController _playerController;
+    private Rigidbody _playerRigidbody;
 
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
+        _playerRigidbody = GetComponent<Rigidbody>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
             collectible.Collect();
         }
-
     }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.TryGetComponent<IBoostable>(out var boostable))
         {
             boostable.Boost(_playerController);
         }
+    }
 
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+        {
+            damageable.GiveDamage(_playerRigidbody, _playerVisualTransform);
+        }
     }
 }
-
